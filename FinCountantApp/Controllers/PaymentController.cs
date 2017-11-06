a3ef09cca3e137d143e763e36f0015c74e86b3f4
Fincountant <33073614+Fincountant@users.noreply.github.com>
2017-11-06 16:14:28 +0500

    aaa

---
+++ FinCountantApp/Controllers/PaymentController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FinCountantApp.Controllers
+{
+    public class PaymentController : ApiController
+    {
+        private string GetConnectionString()
+        {
+            return System.Configuration.ConfigurationManager.ConnectionStrings["FinDBString"].ConnectionString;
+        }
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "iqbal", "value2" };
+        }
+
+        [HttpPost]
+        public void paymentData()
+        {
+            string data = "hello world";
+            SqlConnection conn = new SqlConnection(GetConnectionString());
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("insert into testTable (testTableData) values(@thisData)", conn);
+            cmd.Parameters.AddWithValue("@thisData", data);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ FinCountantApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinCountantApp.WebApiApplication" Language="C#" %>
